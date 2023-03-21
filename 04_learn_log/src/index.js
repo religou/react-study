570ba4c1f589189121e05f9e11d9fc1c10e46540
erkang zhou <3226778019@qq.com>
2023-03-21 20:33:22 +0800

    add css

--- 04_learn_log/src/index.js
+++ 04_learn_log/src/index.js
@@ -1,14 +1,27 @@
 import reactDOM from "react-dom/client"
+import "./index.css"
 
 const comp = <div className="log">
   <div className="item">
     <div className="date">
-      <div className="month">2023年03</div>
+      <div className="month">2023年03月</div>
       <div className="day">20</div>
     </div>
 
     <div className="content">
-      <div className="learn-content">学习React</div>
+      <div className="desc">学习React</div>
+      <div className="during">30分钟</div>
+    </div>
+  </div>
+
+  <div className="item">
+    <div className="date">
+      <div className="month">2023年03月</div>
+      <div className="day">20</div>
+    </div>
+
+    <div className="content">
+      <div className="desc">学习React</div>
       <div className="during">30分钟</div>
     </div>
   </div>
