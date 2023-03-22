a213f467fa6c00318849512969adb554dc7a1b4e
erkang zhou <3226778019@qq.com>
2023-03-22 20:32:56 +0800

    使用组件的方式重写练习

---
+++ 04_learn_log/05_函数组件/index.js
@@ -0,0 +1,6 @@
+import reactDOM from "react-dom/client"
+import App from './App'
+
+const root = reactDOM.createRoot(document.getElementById("root"))
+// 把 App 当作组件在根组件中渲染出来
+root.render(<App></App>)
